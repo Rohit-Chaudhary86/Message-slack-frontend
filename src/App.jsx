cd5c097511384554f15969d5be45c0e1c1dff037
Rohit chaudhary <151979116+Rohit-Chaudhary86@users.noreply.github.com>
2026-02-08 15:18:59 +0530

    added shard cn

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Button } from "@/components/ui/button"
+import { useState, useEffect } from "react"
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(
+    () => Number(localStorage.getItem("count")) || 0
+  )
+
+  const [minus, setMinus] = useState(
+    () => Number(localStorage.getItem("minus")) || 10
+  )
+
+  useEffect(() => {
+    localStorage.setItem("count", count)
+    localStorage.setItem("minus", minus)
+  }, [count, minus])
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="flex min-h-svh flex-col items-center justify-center gap-4">
+      <Button onClick={() => setCount(count + 1)}>
+        Click me {count}
+      </Button>
+
+      <Button onClick={() => setMinus(minus - 1)}>
+        Click me {minus}
+      </Button>
+    </div>
   )
 }
 
